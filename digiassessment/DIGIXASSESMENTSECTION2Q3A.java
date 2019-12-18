d2beed72055f041bd4e72a357b3c2f7299190395
azhaddhilmi <33600767+azhaddhilmi@users.noreply.github.com>
2019-12-18 14:53:15 +0800

    Add files via upload
    run:
    trueBUILD SUCCESSFUL (total time: 0 seconds)

---
+++ digiassessment/DIGIXASSESMENTSECTION2Q3A.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package digixassesmentsection2q3.a;
+
+/**
+ *
+ * @author Rocky
+ */
+public class DIGIXASSESMENTSECTION2Q3A {
+// returns true if string is a palindrome 
+    static boolean isPalindrome(String string) 
+    { 
+  
+        
+        int i = 0, j = string.length() - 1; 
+  
+        while (i < j) { 
+  
+            
+            if (string.charAt(i) != string.charAt(j)) 
+                return false; 
+  
+            // Increase the first pointer and decrement the other 
+            i++; 
+            j--; 
+        } 
+  
+        // returned string is a palindrome 
+        return true; 
+    } 
+  
+    
+    
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String string = "tacocat"; 
+  
+        if (isPalindrome(string)) 
+            System.out.print("true"); 
+        else
+            System.out.print("false"); 
+        
+    }
+    
+}
