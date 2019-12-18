4a614992306d82aad13dce3b3d131919c7cbe757
azhaddhilmi <33600767+azhaddhilmi@users.noreply.github.com>
2019-12-18 15:12:30 +0800

    Add files via upload

---
+++ digiassessment/DIGIXASSESSMENTS2Q3D.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package digixassessments2q3.d;
+
+/**
+ *
+ * @author Rocky
+ */
+public class DIGIXASSESSMENTS2Q3D {
+  // returns true if string is a palindrome 
+    static boolean isPalindrome(String string) 
+    { 
+  
+        
+        int i = 0, j = string.length() - 1; 
+  
+        while (i < j) { 
+            if (string.charAt(i) != string.charAt(j)) 
+                return false; 
+  
+            // Increase the first pointer and decrement the other 
+            i++; 
+            j--; 
+        } 
+  
+        // returned string is a palindrome 
+        return true; 
+    } 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         String string = "Hannah"; 
+  
+        if (isPalindrome(string)) 
+            System.out.print("true"); 
+        else
+            System.out.print("false");
+    }
+    
+}
